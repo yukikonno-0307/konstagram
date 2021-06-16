bb4ee44892801b3d9155f82a0ffeb00a005b2e47
yukikonno-0307 <konnoyuki.dev@gmail.com>
2021-06-16 10:17:39 +0000

    Add users model by devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  devise_for :users
   #get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
